34409a463df26231160834281717849b69111de7
fachriza234 <farizsmi0123@gmail.com>
2022-07-19 10:18:15 +0700

    JAKOB SITORUS

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
-
 const Hapi = require('@hapi/hapi');
 
+// authentication
+const authentication = require('./api/authentication');
+const Database = require('./conf/Database');
+const ClientError = require('./exceptions/ClientError');
+const AuthenticationService = require('./services/mysql/AuthenticationService');
+const AuthenticationValidator = require('./validator/authentication');
+
 const init = async () => {
+  const database = new Database();
+  const authenticationService = new AuthenticationService(database);
 
   const server = Hapi.server({
     host: process.env.HOST,
@@ -18,8 +26,37 @@ const init = async () => {
     method: 'GET',
     path: '/',
     handler: () => ({
-      name: 'JAKOB SITORUS',
+      name: 'Jakob Sitorus',
     }),
+  });
+
+  // defines internal plugins
+  await server.register([
+    {
+      plugin: authentication,
+      options: {
+        service: authenticationService,
+        validator: AuthenticationValidator,
+      },
+    },
+  ]);
+
+  // extension
+  server.ext('onPreResponse', (request, h) => {
+    const {response} = request;
+
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    console.log(response);
+
+    return h.continue;
   });
 
   await server.start();
@@ -27,4 +64,4 @@ const init = async () => {
 
 };
 
-init()
+init();
