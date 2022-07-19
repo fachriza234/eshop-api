ee3bd6f21f8f4bfee00945924d6629a997fa43a1
fachriza234 <farizsmi0123@gmail.com>
2022-07-19 11:50:53 +0700

    JAKOB SITORUS

--- src/api/authentication/handler.js
+++ src/api/authentication/handler.js
@@ -1,18 +1,24 @@
+const Jwt = require('@hapi/jwt');
+
 class AuthenticationHandler {
-  #service;
-  #validator;
+    #service;
+    #validator;
+  
+    constructor(service, validator) {
+      this.#service = service;
+      this.#validator = validator;
+  
+      this.postRegister = this.postRegister.bind(this);
+      this.postLogin = this.postLogin.bind(this);
+      this.getUser = this.getUser.bind(this);
+    }
 
-  constructor(service, validator) {
-    this.#service = service;
-    this.#validator = validator;
-
-    this.postRegister = this.postRegister.bind(this);
-    this.postLogin = this.postLogin.bind(this);
-    this.getUser = this.getUser.bind(this);
-  }
-
-  async postRegister(request, h) {
-    this.#validator.validateRegisterPayload(request.payload);
+    #generateToken(payload) {
+      return Jwt.token.generate(payload, process.env.TOKEN_KEY)
+    }
+  
+    async postRegister(request, h) {
+        this.#validator.validateRegisterPayload(request.payload);
     const { email, name, password } = request.payload;
 
     const id = await this.#service.register(email, name, password);
@@ -26,25 +32,32 @@ class AuthenticationHandler {
     });
     response.code(201);
     return response;
-  }
 
-  async postLogin(request, h) {
-    this.#validator.validateLoginPayload(request.payload);
-    const { email, password } = request.payload;
+    }
+  
+    async postLogin(request, h) {
+        this.#validator.validateLoginPayload(request.payload);
+        const { email, password } = request.payload;
+    
+        const { id } = await this.#service.login(email, password)
 
-    const { id } = await this.#service.login(email, password)
+        
+        const payloadToken = { id, email };
+        const token = this.#generateToken(payloadToken);
 
-    return {
-      status: 'success',
-      message: 'User berhasil login',
-      data: {
-        id,
-      },
-    };
-  }
-
-  async getUser(request, h) {
-    const { id } = request.params;
+    
+        return {
+          status: 'success',
+          message: 'User berhasil login',
+          data: {
+            id,
+            token,
+          },
+        };
+    }
+  
+    async getUser(request, h) {
+        const { id } = request.params;
     const user = await this.#service.getUserById(id);
 
     return {
@@ -54,7 +67,7 @@ class AuthenticationHandler {
         user,
       },
     };
+    }
   }
-}
-
-module.exports = AuthenticationHandler;
+  
+  module.exports = AuthenticationHandler;
