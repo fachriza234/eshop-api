97bb1abb44356071b400c43c291d525ce742d9cc
fachriza234 <farizsmi0123@gmail.com>
2022-07-19 11:04:45 +0700

    JAKOB SITORUS

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 
 // authentication
 const authentication = require('./api/authentication');
@@ -27,6 +28,34 @@ const init = async () => {
     path: '/',
     handler: () => ({
       name: 'Jakob Sitorus',
+    }),
+  });
+
+ 
+
+  
+  
+
+   // register external plugin
+   await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  // defines authentication strategy
+  server.auth.strategy('eshop_jwt', 'jwt',{
+    keys: process.env.TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
     }),
   });
 
